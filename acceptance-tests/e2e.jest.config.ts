4b694c4dbeab97668186b3d8cea787176addb27d
Eduardo <EduardoAC@users.noreply.github.com>
2024-06-04 15:42:13 +0100

    Review tests

--- acceptance-tests/e2e.jest.config.ts
+++ acceptance-tests/e2e.jest.config.ts
@@ -1,9 +1,9 @@
 // jest.config.ts
-import { fileURLToPath } from "url"
-import { dirname } from "path"
+// import { fileURLToPath } from "url"
+// import { dirname } from "path"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
 
 const pathToExtension = `${__dirname}/dist`
 
--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,29 @@ import { type JestConfigWithTsJest } from "ts-jest"
 // jest.config.ts
 
 const config: JestConfigWithTsJest = {
-    projects: ["acceptance-tests/jest.config.ts"],
+    // verbose: true, // More in-depth information on time and tests that execute
+    preset: "ts-jest",
+    testEnvironment: "jest-environment-jsdom",
+    automock: false,
+    setupFilesAfterEnv: ["./setupTests.ts"],
+    testMatch: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)",
+    ],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+        // process `*.tsx` files with `ts-jest`
+    },
+    moduleNameMapper: {
+        "\\.css$": "jest-css-modules",
+    },
+    // reporters: [
+    //     [
+    //         // To investigate to learn why a particular test is slow
+    //         "jest-slow-test-reporter",
+    //         { numTests: 8, warnOnSlowerThan: 1000, color: true },
+    //     ],
+    // ],
 }
 
 export default config
